a77e9c64a9904638e20698760c8be6d6f310f7e1
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-02-14 18:52:54 -0600

    cw 3 solution

--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,7 +1,9 @@
 //write includes statements
+#include<iostream>
+#include "loops.h"
 
 //write using statements for cin and cout
-
+using std::cin; using std::cout;
 
 /*
 Use a do while loop to prompt the user for 
@@ -10,6 +12,22 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
+	int choice;
+
+	do 
+	{
+		int num, result;
+		cout << "Enter a number: ";
+		cin >> num;
+		result = factorial(num);
+
+		cout << "Factorial of " << num << " is " << result << "\n";
+
+		cout << "Enter 1 to continue...";
+		cin >> choice;
+
+	} while (choice == 1);
+
 
 	return 0;
 }
