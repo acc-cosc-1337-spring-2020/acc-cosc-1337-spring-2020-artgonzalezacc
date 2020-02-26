69566460c71ad979a46ee0bc84a533071b449318
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-02-25 19:41:21 -0600

    OO BankAccount examples and test cases

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,6 +1,20 @@
+#include "bank_account.h"
+#include<iostream>
+
+using std::cout; using std::cin;
 
 int main()
 {
+	BankAccount account(500);
+	auto balance = account.get_balance();
+	cout << "Balance is: \n" << balance;
+
+	auto amount{ 0 };
+	cout << "\nEnter deposit amount: \n";
+	cin >> amount;
+	
+	account.deposit(amount);
+	cout << "Balance is: " << account.get_balance();
 
 	return 0;
 }
