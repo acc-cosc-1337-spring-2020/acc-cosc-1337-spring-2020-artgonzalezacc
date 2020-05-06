040809f6ab630259ea281882f81843de9c81503a
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-05-06 18:33:34 -0500

    Stack arrays and free functions

--- src/examples/12_module/05_arrays_functions/arr_functions.cpp
+++ src/examples/12_module/05_arrays_functions/arr_functions.cpp
@@ -2,3 +2,13 @@
 #include<iostream>
 #include "arr_functions.h"
 //define iterate_array and loop through with ++ increment
+#include<iostream>
+
+void iterate_array(int * nums, const int SIZE)
+{
+	
+	for (int i = 0; i < SIZE; ++i) 
+	{
+		std::cout << nums[i] << "\n";
+	}
+}
